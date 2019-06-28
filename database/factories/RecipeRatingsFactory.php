422d4019684587ef369943619d0e38bac1469096
Justin Laureano <gravatron87@gmail.com>
2019-06-28 19:34:52 -0400

    re-weighted rating seed randomizer

--- database/factories/RecipeRatingsFactory.php
+++ database/factories/RecipeRatingsFactory.php
@@ -7,7 +7,7 @@ use Faker\Generator as Faker;
 
 $factory->define(RecipeRatings::class, function (Faker $faker) {
     // weighted to give more high scores
-    $ratings = [0, 0.5, 1, 1.5, 2, 2.5, 2.5, 3, 3, 3, 3.5, 3.5, 3.5, 4, 4, 4, 4, 4.5, 4.5, 5, 5];
+    $ratings = [0, 0.5, 1, 1.5, 2, 2.5, 2.5, 2.5, 3, 3, 3, 3, 3, 3.5, 3.5, 3.5, 3.5, 4, 4, 4, 4, 4, 4, 4.5, 4.5, 4.5, 5, 5];
     return [
         'recipe_id' => 1,
         'rating' => $faker->randomElement($ratings),
