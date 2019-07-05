bddf89172f9f8e1dd1564df4fa8a00b2bfe28ccd
Justin Laureano <gravatron87@gmail.com>
2019-07-05 15:32:51 -0400

    styling public

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,37 +1,62 @@
 @extends('layouts.public')
 
 @section('content')
-<div class="">
-    <div class="card-header">{{ __('Login') }}</div>
-
-    <form method="POST" action="{{ route('login') }}">
+<section class="login">
+    <form 
+        action="{{ route('login') }}"
+        class="login-form"
+        method="POST"> 
         @csrf
-        <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-        @error('email')
-            <span class="invalid-feedback" role="alert">{{ $message }}</span>
-        @enderror
 
-        <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-        @error('password')
-            <span class="invalid-feedback" role="alert">{{ $message }}</span>
-        @enderror
+        <h2 class="login-form__title">Sign In</h2>
+        <a class="login-form__create" href="{{ route('register') }}">Create Account</a>
 
-        <input class="" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-        <label class="" for="remember">
-            {{ __('Remember Me') }}
-        </label>
+        <section class="login-form__input--email">
+            <input 
+                id="email" 
+                type="email" 
+                class="input @error('email') is-invalid @enderror" 
+                name="email" 
+                value="{{ old('email') }}" 
+                required autocomplete="email"
+                placeholder="E-mail Address"
+                autofocus>
+                @error('email')
+                    <span class="invalid-feedback" role="alert">{{ $message }}</span>
+                @enderror
+        </section>
 
-        <button type="submit" class="btn">
+        <section class="login-form__input--password">
+            <input 
+                id="password" 
+                type="password" 
+                class="input @error('password') is-invalid @enderror" 
+                name="password" 
+                value="{{ old('password') }}" 
+                required autocomplete="password"
+                placeholder="Password"
+                autofocus>
+                @error('password')
+                    <span class="invalid-feedback" role="alert">{{ $message }}</span>
+                @enderror
+        </section>
+
+        <section class="login-form__checkbox--remember">
+            <input class="" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+            <label class="" for="remember">
+                {{ __('Remember Me') }}
+            </label>
+        </section>
+
+        <button type="submit" class="login-form__login btn">
             {{ __('Login') }}
         </button>
 
         @if (Route::has('password.request'))
-            <a class="btn" href="{{ route('password.request') }}">
+            <a class="login-form__forgot" href="{{ route('password.request') }}">
                 {{ __('Forgot Your Password?') }}
             </a>
         @endif
     </form>
-</div>
+</section>
 @endsection
