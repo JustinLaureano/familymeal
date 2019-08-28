59306356c65585d71976137cec0f40f85c722e4d
unknown <gravatron87@gmail.com>
2019-08-28 16:45:57 -0400

    adding and styling footer links

--- resources/views/public/components/footer.blade.php
+++ resources/views/public/components/footer.blade.php
@@ -1,3 +1,5 @@
 <footer class="footer--public">
+    <a class="footer__link--public" href="/privacy">Privacy</a>
+    <a class="footer__link--public" href="/terms">Tems</a>
     <span class="footer__copyright">&copy; 2019 Recipe Confidential</span>
 </footer>
